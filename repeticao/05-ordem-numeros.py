051c7c1a560295751df42113bcc972d76ee568f2
MathsLima <matheuslima13@hotmail.com>
2024-02-26 12:03:02 -0300

    while com print abaixo de ao lado

---
+++ repeticao/05-ordem-numeros.py
@@ -0,0 +1,16 @@
+"""Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro."""
+
+num = 0
+while num <= 19:
+    num = num + 1
+    print(num)
+
+print("\n")
+
+
+cont = 1
+while cont <= 20:
+    print(cont, end=' ')
+    cont = cont + 1
+
