7ab474ff75705ef04e9694e861c2b3a6baf6bd15
MathsLima <matheuslima13@hotmail.com>
2024-02-26 11:20:32 -0300

    nova solucao

--- repeticao/03-dados-usuario.py
+++ repeticao/03-dados-usuario.py
@@ -7,17 +7,47 @@ Sexo: 'f' ou 'm';
 """
 while True:
     nome = input("Digite o nome: ")
-    """idade = input("Digite o idade: ")
-    salario = input("Digite o salario: ")
-    sexo = input("Digite o sexo: ")
-    """
     if len(nome)  > 3:
-        nome == nome
+        print("Nome ", nome)
         break
     else:
         print("Digite um nome com mais de 3 caracteres")
 
-    
 
-print(nome)
+while True:
+    idade = input("Digite sua idade: ")
+    idade = int(idade)
+    if idade > 0 and idade < 150:
+        print("Idade ", idade)
+        break
+    else:
+        print("Digite uma idade entre 0 e 150 anos")
+
+
+while True:
+    salario = input("Digite seu salario: ")
+    salario = int(idade)
+    if salario > 0:
+        salario == salario
+        print("Salario de ", salario)
+        break
+    else:
+        print("Digite um salario maior que 0")
+
+
+while True:
+    sexo = input("Digite seu sexo: ")
+    if sexo == "m" or sexo == "f":
+        print("Sexo ", sexo)
+        break
+    else:
+        print("Digite um sexo valido")
+   
+
+print("Dados:")
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Salario: ", salario)
+print("Salario: ", salario)
+
     
